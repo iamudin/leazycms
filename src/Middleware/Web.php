8d2387b1ac3e8cd1453f8b4a5eaa889627982b95
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2024-08-08 12:08:28 +0700

    Versi up

--- src/Middleware/Web.php
+++ src/Middleware/Web.php
@@ -37,7 +37,7 @@ class Web
             if(get_option('site_maintenance')=='N'){
                 $content = preg_replace('/\s+/', ' ', $content);
             }
-            $footerCredits = '<footer style="text-align:center;background:#000;padding:10px;color:#ccc" class="'.str()->random(5).'_credit"><small>Build with <span style="color:red">Lara</span><b style="color:#fff">mix<b><sup style="color:#f5f5f4">.id</sup></small></footer>';
+            $footerCredits = '<footer style="text-align:center;background:#000;padding:10px;color:#ccc" class="'.str()->random(5).'_credit">Leazycms 1.0.1</footer>';
             $content = preg_replace('/<\/body>/', $footerCredits . '</body>', $content);
             $response->setContent($content);
         }
