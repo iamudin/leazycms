cc2b4c6c36524d81c66291584a682cae00a7342a
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2024-08-14 10:11:21 +0700

    bercategory

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -12,7 +12,7 @@ class Category extends Model
 
     public function posts()
     {
-    return $this->hasMany(Post::class)->select((new Post)->selected);
+    return $this->hasMany(Post::class);
     }
     public function medias()
     {
