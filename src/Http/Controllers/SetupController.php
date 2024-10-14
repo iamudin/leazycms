1c92efc765d8b9e8add3bb864f2870cf204bdc25
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2024-10-14 08:18:29 +0700

    update paginate

--- src/Http/Controllers/SetupController.php
+++ src/Http/Controllers/SetupController.php
@@ -65,6 +65,7 @@ class SetupController extends Controller
                     $db['DB_PASSWORD'] = '';
                 }
                 $this->createEnvConfig($db);
+                Artisan::call('vendor:publish --tag=laravel-pagination');
                 return to_route('initializing');
             }
         }
@@ -83,14 +84,12 @@ class SetupController extends Controller
                     'name' => $k
                 ], ['value' => $row, 'autoload' => 1]);
             }
-            regenerate_cache();
-
             clear_route();
             if ($this->createEnvConfig(['APP_INSTALLED' => true])) {
                 Artisan::call('vendor:publish --tag=cms');
-                Artisan::call('vendor:publish --tag=laravel-pagination');
                 Artisan::call('optimize');
                 Cache::forget('dbcredential');
+                Cache::forget('option');
                 Cache::forget('usercredential');
                 return to_route('login');
             }
