a8b4e61ed90f5e7705abc501362636f349a1fab6
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2024-12-06 10:56:31 +0700

    login form

--- src/views/auth/login.blade.php
+++ src/views/auth/login.blade.php
@@ -12,13 +12,12 @@
     <title>Masuk - {{get_option('site_title')}}</title>
     <link rel="shortcut icon" href="{{ url('favicon.ico') }}" />
     <meta property="og:title" content="Masuk - {{get_option('site_title')?? 'Web Title'}}" />
-<meta property="og:image" content="{{url(get_option('logo')??noimage())}}" />
+<meta property="og:image" content="{{url(get_option('logo') ?? noimage())}}" />
 <meta property="og:site_name" content="{{get_option('site_title') ?? 'Web Title'}}" />
 <meta property="og:description" content="Masuk Sebagai Admin / Operator" />
   </head>
   <body>
-
-    <section class="login-content" style="background:#000">
+    <section class="login-content" style="background:#1f1f1f">
       <div class="login-box" style="background:transparent;box-shadow:none;width:100%">
 
         <form method="POST"  style="width:300px;margin-left:auto;margin-right:auto"  action="{{route('login.submit') }}">
@@ -28,10 +27,11 @@
             <br>
             <br>
             <h4 class="text-warning">{{get_option('site_title') ?? 'Web Title'}}</h4>
+           @if(get_option('site_description')) <h6 class="text-muted"><i>{{ get_option('site_description') }}</i></h6>@endif
             <br>
 
             @if(get_option('site_maintenance')=='Y')
-            <p class="badge badge-danger">Modus Perbaikan Aktif</p>
+            <p class="badge badge-danger">Mode Perbaikan Aktif</p>
             @endif
           </center>
                 @if (session()->has('error'))
@@ -41,16 +41,16 @@
                 </div>
                 @endif
 
-          <div class="form-group">
-            <label class="control-label" style="color:#f5f5f5">Nama Pengguna</label>
-                <input id="username" placeholder="Username" type="text" class="form-control form-control-lg " name="username" required autocomplete="username" autofocus>
+          <div class="form-group  pb-0 mb-2">
+            <label class="control-label" style="color:#f5f5f5">Username</label>
+                <input id="username" onkeyup="this.value = this.value.replace(/\s+/g, '')" placeholder="Enter Username" type="text" class="form-control form-control-lg " name="username" required autocomplete="username" autofocus>
           </div>
           <div class="form-group">
-            <label class="control-label" style="color:#f5f5f5">Kata Sandi</label>
-                <input id="password" placeholder="*****" type="password" class="form-control form-control-lg " name="password" required autocomplete="current-password" autofocus>
+            <label class="control-label" style="color:#f5f5f5">Password</label>
+                <input id="password" onkeyup="this.value = this.value.replace(/\s+/g, '')" placeholder="Enter Password" type="password" class="form-control form-control-lg " name="password" required autocomplete="current-password" autofocus>
           </div>
           <div class="form-group">
-            <img src="{{ $captcha }}" alt=""> <input type="text" name="captcha" placeholder="Masukkan Kode..." required  style="border:none;float:right;height: 40px;">
+            <img src="{{ $captcha }}" alt="" style="border-radius: 5px 0 0 5px"> <input type="text" name="captcha" placeholder="Enter Code" required  style="border:none;float:right;height: 40px;border-radius:0 5px 5px 0">
           </div>
 
           <div class="form-group btn-container">
