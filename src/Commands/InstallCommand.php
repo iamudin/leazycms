e308c9b1f6a837b5106ca2647fb00a62f7fa81de
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-02-14 09:51:10 +0700

    udpate comment

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -60,6 +60,7 @@ class InstallCommand extends Command
                 "APP_URL" => "http://".$domain,
                 "CACHE_STORE" => "file",
                 "SESSION_DRIVER" => "file",
+                "APP_TIMEZONE" => "Asia/Jakrta",
             ]);
 
             // Bersihkan cache
