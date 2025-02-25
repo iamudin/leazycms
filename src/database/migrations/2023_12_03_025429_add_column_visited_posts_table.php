12a47f033760e42d53ba658aa539b24b6332c7cf
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-02-25 13:47:25 +0700

    sdfsdfsdf

--- src/database/migrations/2023_12_03_025429_add_column_visited_posts_table.php
+++ src/database/migrations/2023_12_03_025429_add_column_visited_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->integerIncrements('visited')->default(0)->nullable();
+            $table->mediumInteger('visited')->default(0)->nullable();
 
         });
     }
