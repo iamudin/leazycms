1d8678a7f854ad0ad6f5bb62d9fe5b1ba26fef18
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-04-09 09:46:59 +0700

    sdafdsf

--- src/Inc/Helpers.php
+++ src/Inc/Helpers.php
@@ -1231,22 +1231,7 @@ if (!function_exists('banner_here')) {
 }
 
 function get_client_ip() {
-    $ipaddress = '';
-    if (isset($_SERVER['HTTP_CLIENT_IP']))
-        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_X_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-    else if(isset($_SERVER['HTTP_FORWARDED']))
-        $ipaddress = $_SERVER['HTTP_FORWARDED'];
-    else if(isset($_SERVER['REMOTE_ADDR']))
-        $ipaddress = $_SERVER['REMOTE_ADDR'];
-    else
-        $ipaddress = 'UNKNOWN';
-    return $ipaddress;
+    return request()->header('CF-Connecting-IP') ?? request()->getClientIp();
   }
 
 if (!function_exists('get_ip_info')) {
