cdbe5b22ff2470de8ca9d5d87dca01dd8b9723a1
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-05-03 15:35:07 +0700

    dsfdsf

--- src/Inc/Helpers.php
+++ src/Inc/Helpers.php
@@ -143,7 +143,7 @@ if (!function_exists('isNotInSession')) {
 if (!function_exists('forbidden')) {
     function forbidden($request, $k = false)
     {
-        if($request->segment(1)!=admin_path()){
+        if($request->segment(2)!='appearance'){
         if (get_option('forbidden_keyword') && str()->contains(str($request->fullUrl())->lower(), explode(",", str_replace(" ", "", get_option('forbidden_keyword') ?? '')))) {
             $redirect = get_option('forbidden_redirect');
             if (!$k) {
