841c587fb18e7fa96d2be76954ca4ea9ce79a189
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-05-26 07:31:44 +0700

    decode url

--- src/Middleware/RateLimit.php
+++ src/Middleware/RateLimit.php
@@ -65,7 +65,7 @@ class RateLimit
             $redirectUrl = $scheme . '://' . $appUrlHost . $uri;
         }
 
-        if ($redirectUrl && rtrim($redirectUrl,'/') !== $request->fullUrl()) {
+        if ($redirectUrl && rtrim($redirectUrl,'/') !== urldecode($request->fullUrl())) {
             return redirect($redirectUrl, 301);
         }
         $modules = collect(get_module())->where('name', '!=', 'page')->where('public', true);
