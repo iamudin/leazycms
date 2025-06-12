eafaa3a98e98e374d5b334704785cb229fd43fac
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-06-12 09:00:06 +0700

    dsfsdfsd

--- src/Http/Controllers/NotifReader.php
+++ src/Http/Controllers/NotifReader.php
@@ -1,25 +1,31 @@
 <?php
 namespace Leazycms\Web\Http\Controllers;
 use Illuminate\Http\Request;
-
+use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Routing\Controllers\HasMiddleware;
 use App\Http\Controllers\Controller;
 use Leazycms\Web\Models\Notification;
 
-class NotifReader extends Controller
-{
+class NotifReader extends Controller  implements HasMiddleware
 
+{
+    public static function middleware(): array {
+        return [
+            new Middleware('auth')
+        ];
+    }
       function notifreader($notification){
         $notification = Notification::whereId($notification)->first();
         if(empty($notification)){
         return to_route('login');
 
         }
-        if(auth()->check() && auth()->user()->isAdmin() || $notification->user_id == auth()->user()->id){
-
+        if(auth()->user()->isAdmin() || $notification->user_id == auth()->id){
             $notification->mark_as_read();
             return redirect($notification->url);
         }
         return to_route('login');
+
     }
 }
 
