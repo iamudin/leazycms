ff85017c58c34f10f793cdd5a614c0ec72ad3187
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-10-08 09:27:46 +0700

    update

--- src/views/backend/dashboard.blade.php
+++ src/views/backend/dashboard.blade.php
@@ -21,7 +21,7 @@
             @endforeach
           </div>
   </div>
-  <div class="col-lg-8 mb-3">
+  <div class="col-lg-12 mb-3">
     <div class="card" style="padding:15px">
     <h4 for="" style="margin-bottom:20px"><i class="fa fa-pencil" aria-hidden="true"></i> 5 Terakhir Dibuat</h4>
     <div class="table-responsive">
@@ -48,26 +48,10 @@
     </div>
   </div>
   </div>
-  <div class="col-lg-4">
- 
-  </div>
   <div class="col-lg-12 mt-3">
   @include('cms::backend.stats')
   </div>
 
   </div>
 
-  @push('styles')
-
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/rowreorder/1.4.1/css/rowReorder.dataTables.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.dataTables.min.css">
-
-  @endpush
-  @push('scripts')
-  <script type="text/javascript" src="{{secure_asset('backend/js/plugins/jquery.dataTables.min.js')}}"></script>
-       <script type="text/javascript" src="{{secure_asset('backend/js/plugins/dataTables.bootstrap.min.js')}}"></script>
-       <script type="text/javascript" src="https://cdn.datatables.net/rowreorder/1.4.1/js/dataTables.rowReorder.min.js"></script>
-       <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
-       <script type="text/javascript">$('#sampleTable').DataTable();</script>
-  @endpush
 @endsection
