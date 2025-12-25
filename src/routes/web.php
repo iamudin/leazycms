2f018882356e8c55e6c490135fadd89b8b04fb8a
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-12-25 15:28:48 +0700

    upd

--- src/routes/web.php
+++ src/routes/web.php
@@ -52,7 +52,7 @@ $modules = collect(get_module())->where('name','!=','page')->where('active', tru
     }
 Route::match(['get', 'post'], '/{slug}', [WebController::class, 'detail'])
     ->where('slug', '(?!' . implode('|', array_merge(
-        [admin_path(), 'search', 'tags', 'install', 'author', 'sitemap.xml', 'favicon.ico'],
+        [admin_path(), 'search', 'tags','log-viewer', 'install', 'author', 'sitemap.xml', 'favicon.ico'],
         $modules->pluck('name')->toArray()
     )) . ')[a-zA-Z0-9-_]+')
     ->middleware(['public']);
