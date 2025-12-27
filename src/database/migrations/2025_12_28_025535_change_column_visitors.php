1eeee47d1bece17ce52c2520100d8fbe9c365c03
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-12-28 06:35:45 +0700

    update lagi

---
+++ src/database/migrations/2025_12_28_025535_change_column_visitors.php
@@ -0,0 +1,20 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+public function up(): void
+{
+        Schema::table('visitor_logs', function (Blueprint $table) {
+            $table->string('reference',1000)->change();
+            $table->string('page',1000)->change();
+        
+        });
+  }
+ public function down(): void
+    {
+
+    }
+
+};
