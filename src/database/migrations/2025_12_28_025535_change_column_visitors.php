f67a34b46b97f2368e80245eb15dea781530f267
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2025-12-28 06:50:18 +0700

    last

--- src/database/migrations/2025_12_28_025535_change_column_visitors.php
+++ src/database/migrations/2025_12_28_025535_change_column_visitors.php
@@ -7,10 +7,20 @@ return new class extends Migration {
 public function up(): void
 {
         Schema::table('visitor_logs', function (Blueprint $table) {
-            $table->string('reference',1000)->nullable()->change();
-            $table->string('page',1000)->change();
-        
+
+            // Ubah panjang kolom
+            $table->string('page', 1000)->change();
+            $table->string('reference', 1000)->nullable()->change();
+
         });
+
+        // Hapus index lama (jika ada)
+        DB::statement('DROP INDEX visitor_logs_page_index ON visitor_logs');
+
+        // Buat PREFIX INDEX (191 karakter aman untuk utf8mb4)
+        DB::statement(
+            'CREATE INDEX visitor_logs_page_index ON visitor_logs (page(191))'
+        );
   }
  public function down(): void
     {
