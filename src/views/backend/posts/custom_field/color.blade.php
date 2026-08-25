4517802fa0a245cd97fc0d26b40ed29878a1a22a
Muhammad Nurul Hudin <m.nurul.hudin@gmail.com>
2026-08-25 14:07:07 +0700

    feat: add color and currency custom field types to post editor

---
+++ src/views/backend/posts/custom_field/color.blade.php
@@ -0,0 +1,27 @@
+@php
+    $colorVal = !empty(old(_us($r[0]))) ? old(_us($r[0])) : (isset($field[_us($r[0])]) && !empty($field[_us($r[0])]) ? $field[_us($r[0])] : '#3b82f6');
+    if (empty($colorVal)) {
+        $colorVal = '#3b82f6';
+    } elseif (!str_starts_with($colorVal, '#') && preg_match('/^[a-fA-F0-9]{6}$/', $colorVal)) {
+        $colorVal = '#' . $colorVal;
+    }
+@endphp
+<small for="{{_us($r[0])}}">{{$r[0]}}</small>
+<div class="input-group">
+    <div class="input-group-prepend">
+        <input type="color"
+            class="form-control p-1"
+            style="width: 44px; height: 38px; cursor: pointer; border-top-right-radius: 0; border-bottom-right-radius: 0;"
+            id="picker_{{_us($r[0])}}"
+            value="{{ $colorVal }}"
+            oninput="document.getElementById('input_{{_us($r[0])}}').value = this.value">
+    </div>
+    <input {{ isset($r[1]->required) ? 'required':'' }} type="text"
+        value="{{ $colorVal }}"
+        class="form-control"
+        id="input_{{_us($r[0])}}"
+        name="{{_us($r[0])}}"
+        placeholder="#3b82f6"
+        maxlength="7"
+        oninput="if(this.value.length === 7 && this.value.startsWith('#')) { document.getElementById('picker_{{_us($r[0])}}').value = this.value; }">
+</div>
---
+++ src/views/backend/posts/custom_field/currency.blade.php
@@ -0,0 +1,19 @@
+@php
+    $currencyVal = !empty(old(_us($r[0]))) ? old(_us($r[0])) : (isset($field[_us($r[0])]) && !empty($field[_us($r[0])]) ? $field[_us($r[0])] : null);
+    if (!empty($currencyVal) && is_numeric(str_replace(['.', ','], '', $currencyVal))) {
+        $currencyVal = number_format((float)str_replace(['.', ','], '', $currencyVal), 0, ',', '.');
+    }
+@endphp
+<small for="{{_us($r[0])}}">{{$r[0]}}</small>
+<div class="input-group">
+    <div class="input-group-prepend">
+        <span class="input-group-text font-weight-bold">Rp</span>
+    </div>
+    <input {{ isset($r[1]->required) ? 'required':'' }} type="text"
+        value="{{ $currencyVal }}"
+        class="form-control"
+        id="{{_us($r[0])}}"
+        name="{{_us($r[0])}}"
+        placeholder="0"
+        oninput="this.value = this.value.replace(/[^0-9]/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, '.')">
+</div>
